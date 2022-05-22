8f69e2a09edd262796df57dbf8c1ca69cceee316
namehcs <65843586+namehcs@users.noreply.github.com>
2022-05-22 12:23:02 +0800

    Add files via upload

---
+++ code-error/algor.cpp
@@ -0,0 +1,527 @@
+#include "algor.h"
+//#include <windows.h>
+
+
+void State_Clear(Data& data, bool clear_all) {
+	if (clear_all) {
+		for (auto& wind : data.window_data){
+			wind.already_installed_device.clear();
+			wind.process_time.clear();
+			wind.forward_intimes = 0;
+			wind.in_times = 0;
+			wind.limit_in = false;
+		}
+		for (auto& area : data.area_data)
+			area.already_installed_device.clear();
+		for (auto& device : data.device_data) {
+			device.install_cost = 0;
+		}
+		data.have_installed_device_num = 0;
+	}
+	else {
+		for (auto& wind : data.window_data) {
+			wind.process_time.clear();
+			wind.forward_intimes = wind.in_times;
+			wind.in_times = 0;
+		}
+	}
+}
+
+void Result::back_trace(vector<vector<int>>& data, int nums, int sp) {
+	if (path_wind.size() == nums) {
+		limit_windows.push_back(path_wind);
+		return;
+	}
+	for (int i = sp; i < data.size(); i++) {
+		path_wind.push_back(data[i][0]);
+		back_trace(data, nums, i + 1);
+		path_wind.pop_back();
+	}
+}
+
+void Result::Sort_Window(Data& data) {
+	/*比较每窗口中安装设备的最大加工时间和最小加工时间差值，该值越大拆环后加工时间减小越多，代价越小*/
+	for (auto& wind : data.window_data) {
+		if (wind.index < data.first_loop_window_num || wind.self_loop) {
+			vector<int> one;
+			if (wind.process_time.size() > 0) {
+				set<int>::iterator end = wind.process_time.end(); end--;
+				one.push_back(wind.index);
+				one.push_back((*end) - (*wind.process_time.begin()));
+			}
+			else {
+				one.push_back(wind.index);
+				one.push_back(0);
+			}
+			unhook_window.push_back(one);
+		}
+	}
+	sort(unhook_window.begin(), unhook_window.end(), [](vector<int>& x, vector<int>& y) { return x[1] > y[1]; });
+
+	for (int nums = 0; nums < unhook_window.size(); nums++) {
+		back_trace(unhook_window, nums, 0);
+	}
+	/*加工时间差值已经按从小到大的顺序存放在map里面了，key为差值，value为窗口号*/
+}
+
+void Result::Backward(Data& data, int& bestline) {
+	long backward_cost;
+	bool backward_valid;
+	/*回溯从后往前调整核心窗口匹配，插入第一个初始化匹配*/
+	installed_window.push_back(installed_window[result_index]);
+	installed_area.push_back(installed_area[result_index]);
+	result_index++;
+	State_Clear(data, false);
+	backward_valid = Install_Match_back(data, result_index);
+	if (backward_valid) {
+		backward_cost = Get_Cost(data);
+		match_costs.push_back(backward_cost);
+	}
+	/*如果回溯代价小于最低代价，则更新最优匹配*/
+	if (backward_valid && backward_cost < match_costs[bestline]) {
+		bestline = result_index;
+	}
+	else if (!backward_valid || backward_cost >= match_costs[bestline]) {
+		result_index--;
+		installed_window.pop_back();
+		installed_area.pop_back();
+		match_costs.pop_back();
+	}
+}
+
+
+bool Result::Forward(Data& data, int& bestline) {
+	bool result = true;
+	/*生成前向匹配，注意这里先要把序列放进data.sqread_circle才能进行后续操作*/
+	/*插入第一个初始化匹配*/
+	long forward_cost, backward_cost;
+	bool forward_valid, backward_valid;
+	installed_window.push_back(vector<int>(data.device_num, 999));
+	installed_area.push_back(vector<int>(data.device_num, 999));
+	result_index++;
+	forward_valid = Install_Match(data, result_index);
+	if (forward_valid) {
+		if (result_index == 0)
+			Sort_Window(data);
+		forward_cost = Get_Cost(data);
+		match_costs.push_back(forward_cost);
+		if (forward_cost < match_costs[bestline])
+			bestline = result_index;
+		Backward(data, bestline);
+	}
+	if (!forward_valid) {
+		result_index--;
+		result = false;
+		installed_window.pop_back();
+		installed_area.pop_back();
+	}
+	State_Clear(data, true);
+	return result;
+}
+
+/**********************************************************************************************
+迭代及优化算法
+**********************************************************************************************/
+void Result::Algorithm(Data& data) {
+	int bestline = 0;
+	long start_time, end_time, process_time;
+	bool forward_result;
+	start_time = clock();
+	forward_result = Forward(data, bestline);
+	State_Clear(data, true);
+	if (forward_result) {
+		/*拆环顺序：
+		1、比较自回环最大次数和第一类环回范围
+		2、如果回环次数小于第一类回环范围：先拆自回环中差值最大的，从大到小(>0,0没必要拆)
+		*/
+		//int unhook_nums = limit_windows.size();
+		for (auto& winds : limit_windows) {
+			for (auto& w : winds)
+				data.window_data[w].limit_in = true;
+			forward_result = Forward(data, bestline);
+			end_time = clock();
+			process_time = (end_time - start_time) / 1000;
+			if (process_time >= 14)
+				break;
+		}
+	}
+	Output(data, bestline);
+	//计算这个res_index的代价，并选择代价最小的匹配结果
+}
+
+
+/**********************************************************************************************
+安装匹配算法
+**********************************************************************************************/
+bool Result::Install_Match(Data& data, int line) {
+	bool valid_result = true;
+	int cur_dev, cur_wind, cur_wind_index = 0;
+	vector<int> done_lastnum(data.device_num, 0);     //过程变量：特殊节点已匹配的输入个数
+	queue<int> L1, L2;
+	/*插入头节点*/
+	for (auto& f : data.linegraph.first_device)
+		L1.push(f->index);
+	while (1) {
+		if (L1.empty()) {
+			L1 = L2;
+			while (!L2.empty())
+				L2.pop();
+			cur_wind_index++;
+			if (cur_wind_index >= data.sqread_circle.size()) {
+				valid_result = false;
+				break;
+			}
+		}
+		cur_dev = L1.front();
+
+		/*判断能否放入当前窗口*/
+		int cur_area;
+		cur_wind = data.sqread_circle[cur_wind_index];
+		bool match_cur_wind = Check_Match(data, cur_dev, cur_wind, cur_wind_index, line, cur_area);
+		if (!match_cur_wind) {
+			L2.push(cur_dev);
+			L1.pop();
+			continue;
+		}
+		install_device(data, cur_dev, cur_area, cur_wind, line, cur_wind_index);  //这里只取了第一个，生成一种匹配方案
+		L1.pop();
+
+		/*判断是否是结束节点*/
+		if (data.device_data[cur_dev].next_device.size() == 0) {
+			if (L1.empty() && L2.empty())
+				break;
+			else
+				continue;
+		}
+
+		/*判断是否是无效窗口序列，不能放下所有设备*/
+		/*后面还有节点，但是已经到了最后一个窗口，如果前后不是协同关系的话就是无效匹配*/
+		if (cur_wind_index == data.sqread_circle.size() - 1){
+			for (auto& next : data.device_data[cur_dev].next_device) {
+				if (data.linegraph.adjacent_matrix[cur_dev][next->index != 2]) {
+					valid_result = false;
+					break;
+				}
+			}
+		}
+		if (!valid_result)//无效匹配，跳出循环
+			break;
+
+		/*判断子节点们是否是特殊节点*/
+		for (auto& son_dev : data.device_data[cur_dev].next_device) {
+			/*不是特殊设备，则将子节点加入L1队列*/
+			if (son_dev->last_device.size() == 1)  //应该不会出现等于0的情况
+				L1.push(son_dev->index);
+			else {
+				/*是特殊设备，则判断子节点所有的输入设备是否都已安装*/
+				if (done_lastnum[son_dev->index] == son_dev->last_device.size() - 1)
+					L1.push(son_dev->index);
+				else
+					done_lastnum[son_dev->index]++;  //没安装完就先等着，等全部安装完了在放入队列进行匹配
+			}
+		}
+	}
+	return valid_result;
+}
+
+
+/**********************************************************************************************
+安装设备，更新状态
+**********************************************************************************************/
+void Result::install_device(Data& data, int device_index, int area_index, int wind, int line, int wind_index) {
+
+	int energy_type = data.area_data[area_index].energy_type;
+	//窗口状态更新
+	data.window_data[wind].already_installed_device.push_back(device_index);
+	//这个设备是核心设备才计算加工时间，process_time改成set，是核心设备就往里push
+	if (data.device_data[device_index].is_core_device) {
+		data.window_data[wind].process_time.insert(data.device_process_time[energy_type]);
+		if(data.device_data[device_index].last_device.size() == 0) //核心头结点的次数要加进去
+			data.window_data[wind].in_times++;
+		for (auto& dev_last : data.device_data[device_index].last_device) {
+			if (dev_last->is_core_device && data.linegraph.adjacent_matrix[dev_last->index][device_index] != 2)
+				data.window_data[wind].in_times++;//这个设备和上个设备是核心设备，且两个设备不是协同设备
+		}
+	}
+	//区域状态更新
+	data.area_data[area_index].already_installed_device.push_back(device_index);
+	//设备状态更新
+	data.device_data[device_index].installed_area = area_index;
+	data.device_data[device_index].install_cost = data.device_data[device_index].energy_install_cost[energy_type];
+	//方案状态更新
+	installed_window[line][device_index] = wind_index;
+	installed_area[line][device_index] = area_index;
+	//全局状态更新
+	data.have_installed_device_num++;
+}
+
+
+long Result::Get_Cost(Data& data) {
+	long install_cost = 0;
+	long preprocess_cost = 0;
+	long window_match_cost = 0;
+	for (auto& device : data.device_data) {
+		install_cost += device.install_cost;
+	}
+	for (auto& window : data.window_data) {  //按最后面的最大值计算加工时间
+		if (window.process_time.size() > 0) {
+			set<int>::iterator end = window.process_time.end(); 
+			end--;
+			preprocess_cost += window.cost_coefficient * (*end); 
+			window_match_cost += (*end) * window.in_times * data.coreline.production_times;
+		}
+	}
+	return install_cost + preprocess_cost + window_match_cost;
+}
+
+
+/**********************************************************************************************
+结果输出
+**********************************************************************************************/
+void Result::Output(Data& data, int line) {
+	/*仪器的数量*/
+	cout << data.device_num << endl;
+
+	/*仪器安装的车间区域下标的数组*/
+	for (int i = 0; i < data.device_num; i++) {
+		cout << installed_area[line][i] << " ";
+	}
+	cout << endl;
+
+	/*流水线的步骤数*/
+	cout << data.coreline.core_devices.size() << endl;
+
+	/*流水线的窗口下标的数组*/
+	int core, wind;
+	for (int i = 0; i < data.coreline.core_devices.size(); i++) {
+		core = data.coreline.core_devices[i];
+		cout << data.sqread_circle[installed_window[line][core]] << " ";
+	}
+	cout << endl;
+}
+
+
+/**********************************************************************************************
+判断当前设备和窗口能不能匹配
+注意：这里的wind_index是窗口号，不是展开后窗口序列编号
+**********************************************************************************************/
+bool Result::Check_Match(Data& data, int dev_index, int wind, int wind_index, int line, int& area_index) {
+	bool result = true;
+	/*检查是否有支持窗口，如果有是否有匹配区域*/
+	if (data.device_data[dev_index].surport_window.find(wind) ==
+		data.device_data[dev_index].surport_window.end())
+		return false;
+	else {
+		if(data.device_data[dev_index].surport_window[wind].size() == 0)
+			return false;
+	}
+
+	/*检查当前设备所有输入设备的窗口索引是否小于当前匹配窗口索引，协同设备可以等于*/
+	for (auto& last_dev : data.device_data[dev_index].last_device) {
+		if (data.linegraph.adjacent_matrix[last_dev->index][dev_index] != 2 &&  //不是协同设备
+			installed_window[line][last_dev->index] >= wind_index)  //而且输入设备的安装窗口
+			return false;
+	}
+
+	/*针对已安装核心设备的窗口进行输入限制：不能再放入核心设备*/
+	if (data.window_data[wind].limit_in && data.device_data[dev_index].is_core_device && 
+		data.window_data[wind].in_times > 0)
+		return false;
+
+	long dev_cost = longmax;
+	/*看每个窗口里支持的区域是否能安装该设备，能安装且费用小于上一个就更新该设备的费用*/
+	for (auto& area : data.window_data[wind].support_area) {
+		int energy_type = data.area_data[area].energy_type;
+		if (data.device_data[dev_index].energy_install_cost[energy_type] > 0 &&
+			data.device_data[dev_index].energy_install_cost[energy_type] < dev_cost) {
+			dev_cost = data.device_data[dev_index].energy_install_cost[energy_type];
+			area_index = area;
+		}
+	}
+
+	return result;
+}
+
+
+/**********************************************************************************************
+窗口匹配算法 基于回溯
+**********************************************************************************************/
+bool Result::Install_Match_back(Data& data, int line) {
+	bool valid_result = true;
+	int cur_dev, cur_wind = 0;
+	int cur_wind_index = data.sqread_circle.size() - 1;
+	vector<int> done_lastnum(data.device_num, 0);     //过程变量：特殊节点已匹配的输入个数
+	queue<int> L1, L2;
+
+	/*插入尾部节点*/
+	for (int f = 0; f < data.linegraph.latest_device.size(); f++)
+		L1.push(data.linegraph.latest_device[f]->index);
+
+	while (1) {
+		if (L1.empty()) {
+			L1 = L2;
+			while (!L2.empty())
+				L2.pop();
+			cur_wind_index--;
+		}
+		if (cur_wind_index < 0) { 
+			break; 
+		}
+		cur_dev = L1.front();
+
+		/*判断能否放入当前窗口*/
+		int cur_area;
+		cur_wind = data.sqread_circle[cur_wind_index];
+		bool match_cur_wind = Check_Match_back(data, cur_dev, cur_wind, cur_wind_index, line, cur_area);
+		if (!match_cur_wind) {
+			L2.push(cur_dev);
+			L1.pop();
+			continue;
+		}
+		int pre_wind = data.sqread_circle[installed_window[line][cur_dev]];
+		int pre_area = installed_area[line][cur_dev];
+		install_device_back(data, cur_dev, cur_area, cur_wind, cur_wind_index, line, pre_wind, pre_area);
+		L1.pop();
+
+		/*判断是否是结束节点*/
+		if (data.device_data[cur_dev].last_device.size() == 0) {
+			if (L1.empty() && L2.empty())
+				break;
+			else
+				continue;
+		}
+
+		/*判断是否是无效窗口序列，不能放下所有设备*/
+		/*后面还有节点，但是已经到了最后一个窗口，如果前后不是协同关系的话就是无效匹配*/
+		if (cur_wind_index == 0) {
+			for (auto& last : data.device_data[cur_dev].last_device) {
+				if (data.linegraph.adjacent_matrix[last->index][cur_dev] != 2) {
+					valid_result = false;
+					break;
+				}
+			}
+		}
+		if (!valid_result)//无效匹配，跳出循环
+			break;
+
+		/*判断子节点们是否是特殊节点*/
+		for (int i = 0; i < data.device_data[cur_dev].last_device.size(); i++) {
+			int son_dev = data.device_data[cur_dev].last_device[i]->index;
+			/*不是特殊设备，则将子节点加入L1队列*/
+			if (data.device_data[son_dev].next_device.size() == 1)  //应该不会出现等于0的情况
+				L1.push(son_dev);
+			else {
+				/*是特殊设备，则判断子节点所有的输入设备是否都已安装*/
+				if (done_lastnum[son_dev] == data.device_data[son_dev].next_device.size() - 1)
+					L1.push(son_dev);
+				else
+					done_lastnum[son_dev]++;  //没安装完就先等着，等全部安装完了在放入队列进行匹配
+			}
+		}
+	}
+	return valid_result;
+}
+
+/**********************************************************************************************
+判断当前设备和窗口能不能匹配（优化版本）
+注意：这里的wind_index是窗口号，不是展开后窗口序列编号
+**********************************************************************************************/
+bool Result::Check_Match_back(Data& data, int dev_index, int wind, int wind_index, int line, int& area_index) {
+	bool result = true;
+	/*检查是否有支持窗口，如果有是否有匹配区域*/
+	if (data.device_data[dev_index].surport_window.find(wind) ==
+		data.device_data[dev_index].surport_window.end())
+		return false;
+	else {
+		if (data.device_data[dev_index].surport_window[wind].size() == 0)
+			return false;
+	}
+
+	/*检查当前设备所有输入设备的窗口索引是否小于当前匹配窗口索引，协同设备可以等于*/
+	for (auto& next_dev : data.device_data[dev_index].next_device) {
+		if (data.linegraph.adjacent_matrix[next_dev->index][dev_index] != 2 &&  //不是协同设备
+			installed_window[line][next_dev->index] <= wind_index)  //而且输入设备的安装窗口
+			return false;
+	}
+
+	/*针对已安装核心设备的窗口进行输入限制：不能再放入核心设备*/
+	if (data.window_data[wind].limit_in && data.device_data[dev_index].is_core_device && 
+		data.window_data[wind].in_times > 0)
+		return false;
+
+	/*遍历这个窗口中安装成本最小的区域是哪个*/
+	int min_cost = longmax, area_;
+	for (auto& area : data.device_data[dev_index].surport_window[wind])
+	{
+		int new_cost = data.device_data[dev_index].energy_install_cost[data.area_data[area].energy_type];
+		if (new_cost < min_cost) {
+			min_cost = new_cost;
+			area_ = area;
+		}
+	}
+
+	/*如果移到这个窗口成本变高了，就不放在这个窗口*/
+	if (min_cost > data.device_data[dev_index].install_cost)
+		return false;
+	area_index = area_;
+
+	return result;
+}
+
+/**********************************************************************************************
+安装设备，更新状态，基于回溯
+**********************************************************************************************/
+void Result::install_device_back(Data& data, int device_index, int area_index, int wind, 
+	int wind_index, int line, int pre_wind_index, int pre_area_index) {
+
+	//int window_index = data.area_data[area_index].window_index;
+	int pre_energy_type = data.area_data[pre_area_index].energy_type;
+	int energy_type = data.area_data[area_index].energy_type;
+
+	/*删除之前安装的状态*/
+	if (pre_wind_index >= 0)
+	{
+		/*窗口状态恢复*/
+		auto it = find(data.window_data[pre_wind_index].already_installed_device.begin(), 
+			data.window_data[pre_wind_index].already_installed_device.end(), device_index);
+		data.window_data[pre_wind_index].already_installed_device.erase(it);
+
+		//是核心设备才恢复
+		if (data.device_data[device_index].is_core_device) {
+			data.window_data[pre_wind_index].process_time.clear();
+			for (auto it_dev : data.window_data[pre_wind_index].already_installed_device) {
+				if (data.device_data[it_dev].is_core_device)
+					data.window_data[pre_wind_index].process_time.insert(data.device_process_time[pre_energy_type]);
+			}
+		}
+		//区域状态恢复
+		auto it_area = find(data.area_data[pre_area_index].already_installed_device.begin(), 
+			data.area_data[pre_area_index].already_installed_device.end(), device_index);
+		data.area_data[pre_area_index].already_installed_device.erase(it_area);
+		//全局状态恢复
+		data.have_installed_device_num--;
+	}
+
+	//窗口状态更新
+	data.window_data[wind].already_installed_device.emplace_back(device_index);
+	//这个设备是核心设备才计算加工时间，process_time改成set，是核心设备就往里push
+	if (data.device_data[device_index].is_core_device) {
+		data.window_data[wind].process_time.insert(data.device_process_time[energy_type]);
+		if (data.device_data[device_index].last_device.size() == 0) //核心头结点的次数要加进去
+			data.window_data[wind].in_times++;
+		for (auto& dev_last : data.device_data[device_index].last_device) {
+			if (dev_last->is_core_device && data.linegraph.adjacent_matrix[dev_last->index][device_index] != 2)
+				data.window_data[wind].in_times++;//这个设备和上个设备是核心设备，且两个设备不是协同设备
+		}
+	}
+	//区域状态更新
+	data.area_data[area_index].already_installed_device.emplace_back(device_index);
+	//设备状态更新
+	data.device_data[device_index].installed_area = area_index;
+	data.device_data[device_index].install_cost = data.device_data[device_index].energy_install_cost[energy_type];
+	//方案状态更新
+	installed_window[line][device_index] = wind_index;
+	installed_area[line][device_index] = area_index;
+	//全局状态更新
+	data.have_installed_device_num++;
+}
---
+++ code-error/data.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <iostream>
+#include <set>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/*
+设备数据结构：设备类型dev_class，设备所属窗口dev_win，设备所属车间dev_workshop，设备所属区域dev_area
+*/
+
+class Window {
+public:
+    int index;  //初始化时读入
+    bool self_loop;  //初始化时读入
+    int workershop_index;  //初始化时读入
+    int cost_coefficient;  //初始化时读入
+    set<int> process_time;
+    int in_times;
+    int forward_intimes;
+    bool limit_in = false;
+
+    vector<bool> preprocess_device;  //初始化时读入
+    set<int> support_energy;  //Data_Choose时读入
+    vector<int> support_area;  //Data_Choose时读入
+    vector<int> already_installed_device;
+
+    Window(int index, int self_loop, int workershop_index, int cost_coefficient) :
+        index(index), self_loop(self_loop), workershop_index(workershop_index), cost_coefficient(cost_coefficient) {}
+    ~Window() {}
+};
+
+class Area {
+public:
+    int index;  //初始化时读入
+    int workershop_index;  //初始化时读入
+    int energy_type;  //初始化时读入
+
+    vector<int> already_installed_device;
+
+    Area(int index, int workershop_index, int energy_type) :
+        index(index), workershop_index(workershop_index), energy_type(energy_type) {}
+    ~Area() {}
+};
+
+class Device {
+public:
+    int index;  //初始化时读入
+    int type;  //初始化时读入
+    int installed_area;
+    int install_cost;
+    bool is_core_device;  //初始化时读入
+    vector<long> energy_install_cost; //初始化时读入
+    vector<Device*> next_device; //graph初始化时读入
+    vector<Device*> last_device; //graph初始化时读入
+
+    set<int> surport_energy; //初始化时读入
+    //set<int> surport_window; //Data_Choose时读入
+    unordered_map<int, vector<int>> surport_window;//窗口+区域
+
+    Device(int index, int type, bool is_core_device) :
+        index(index), type(type), is_core_device(is_core_device) {}
+    ~Device() {}
+};
+
+class CoreLine {
+public:
+    int edge_num;//流水线的边数, 初始化时读入
+    int production_times; //初始化时读入
+    vector<int> edge_array;//流水线的边下标数组,初始化时读入
+    vector<int> core_devices; //初始化时读入
+    vector<int> not_installed_device;
+};
+
+class LineGraph {
+public:
+    int edge_num;   //初始化时读入
+    vector<vector<int>> graph_data;  //初始化时读入
+    vector<vector<int>> adjacent_matrix;  //初始化时读入
+    void Tree_Graph();
+    vector<Device*> first_device;
+    vector<Device*> latest_device;
+};
+
+class Data {
+public:
+    vector<Window> window_data;  //初始化时读入
+    vector<Area> area_data;  //初始化时读入
+    vector<Device> device_data;  //初始化时读入
+
+    void Data_Choose();
+    void Read_file();
+    void Read_file_cin();
+    LineGraph linegraph;
+    CoreLine coreline;
+
+
+public:
+    string in_path = "./case0.in";
+    int workershop_num;  //初始化时读入
+    int max_loop_num;  //初始化时读入
+    int first_loop_window_num;  //初始化时读入
+    int window_num;  //初始化时读入
+    int device_num;  //初始化时读入
+    int have_installed_device_num;  //已经安装的设备数量
+    int area_num;    //初始化时读入
+    vector<int> device_process_time;//仪器使用5种能源的加工时间  初始化时读入
+    
+    /*每次找到最低代价的匹配路径后就swap到key=0的位置*/
+    vector<int> sqread_circle; //Data_Choose时读入
+};
